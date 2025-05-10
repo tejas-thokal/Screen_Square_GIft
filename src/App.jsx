f278bffc108ee24963efd9d22ec9aaae1abc703b
Tejas Thokal <tejasthokal2305@gmail.com>
2025-05-11 01:15:42 +0530

    basic modification

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Navbar from "./Navbar.jsx";
 import Sidebar from "./Sidebar.jsx";
 import Home from "./Home_temp.jsx";
 import Product from "./product.jsx";
-import Work from "./Work.jsx";
 import Footer from "./Footer.jsx"
 import Skills from "./Skills.jsx"
 
@@ -12,7 +11,6 @@ export default function App(){
     <Navbar/>
     <Sidebar/>
     <Product/>
-    <Work/>
     <Skills/>
     <Footer/>
     </>
